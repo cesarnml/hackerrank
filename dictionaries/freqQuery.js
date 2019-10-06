acaaa1d763eda87eb7782041fac579d18a602b80
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-10-05 22:39:34 -0700

    typo

--- dictionaries/freqQuery.js
+++ dictionaries/freqQuery.js
@@ -1,7 +1,7 @@
 /*
 You are given  queries. Each query is of the form two integers described below:
 -  : Insert x in your data structure.
--  : Delete one occurence of y from your data structure, if present.
+-  : Delete one occurrence of y from your data structure, if present.
 -  : Check if any integer is present whose frequency is exactly . If yes, print 1 else 0.
 
 The queries are given in the form of a 2-D array  of size  where  contains the operation, and  contains the data element. For example, you are given array . The results of each operation are:
